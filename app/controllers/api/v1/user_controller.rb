3d1280e96f4816b24d464ca9dc33e1f7aca7cdc9
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-28 18:09:16 -0400

    created some basic stuff but it's not finished

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,3 +1,13 @@
 class Api::V1::UsersController < ApplicationController
 
+  def create
+    
+  end
+
+  private
+
+  def user_params
+    params.permit(:user).require(:username, :email, :password)
+  end
+
 end
