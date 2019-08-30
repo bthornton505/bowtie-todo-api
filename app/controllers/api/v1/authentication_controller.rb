910272691f601c008f742e731bebee55272dd558
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-30 17:22:46 -0400

    created authentication controller to handle authenticating users

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::AuthenticationController < ApplicationController
+  skip_before_action :authorize_request, only: :authenticate
+
+  def authenticate
+    auth_token =
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
+
+  def check_token
+    user = AuthorizeApiRequest.new(request.headers).call[:user]
+    json_response(email: user.email)
+  end
+
+  private
+
+  def auth_params
+    params.permit(:email, :password)
+  end
+  
+end
