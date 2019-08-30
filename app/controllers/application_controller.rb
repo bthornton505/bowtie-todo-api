7af8bbb350ab1d98b03890516ddb48bfa984e1c4
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-30 16:58:14 -0400

    indluded Response/ExceptionHandler modules and created helper method or authorizing requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
+
+  # called before every action on controllers
+  before_action :authorize_request
+  attr_reader :current_user
+
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
+
+  private
+
+  # check for valid request token and return user
+  def authorize_request
+    @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
+    # render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
+
 end
