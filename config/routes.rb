6a9bea577a97264ade956e2067f8b5bbb6cdcf8a
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-30 17:41:05 -0400

    updated the namespace of the api routes to api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  scope :api, default: { format: :json } do
-    
+  namespace :api do
+    namespace :v1 do
+
+      post 'signup', to: 'users#create'
+      post 'auth/login', to: 'authentication#authenticate'
+      get 'auth/check_token', to: 'authentication#check_token'
+
+      post 'find_user', to: 'users#find'
+
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
