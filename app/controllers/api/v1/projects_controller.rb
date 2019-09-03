0cb010ab65daa6160deb10fb27ec25815fd5dc0b
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-02 23:23:37 -0400

    created project controller with index, create, show, and destory actions

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::ProjectsController < ApplicationController
+  before_action :set_user
+
+  def index
+    projects = @user.projects
+    render json: projects
+  end
+
+  def create
+    project = @user.projects.create(project_params)
+    render json: project
+  end
+
+  def show
+    project = @user.recipes.find_by(id: params[:id])
+    render json: project
+  end
+
+  def destroy
+    project = Project.find_by(id: params[:id])
+    project.destroy
+
+    render json: project
+  end
+
+  private
+
+  def project_params
+    params.permit(:title, :user_id)
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+end
