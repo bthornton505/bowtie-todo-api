19de747682687f62e43022bfc11b307964bd9eef
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-02 23:22:46 -0400

    added active record relationship to projects and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :projects
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates_presence_of :password_digest
 end
