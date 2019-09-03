151385b7be7a19937a81a172ddd18dba9bc0b899
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-02 23:23:01 -0400

    added resource routes for projects model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, :projects
 
       post 'signup', to: 'users#create'
       post 'auth/login', to: 'authentication#authenticate'
