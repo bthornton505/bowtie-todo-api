82209d19962a641b4665fe26f273c6d360965232
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-04 12:33:24 -0400

    made some spelling changes

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,34 +3,34 @@ class Api::V1::ProjectsController < ApplicationController
 
   def index
     projects = @user.projects
-    render json: projects
+    json_response(projects)
   end
 
   def create
     project = @user.projects.create(project_params)
-    render json: project
+    json_response(project)
   end
 
   def show
     project = @user.projects.find_by(id: params[:id])
-    render json: project
+    json_response(project)
   end
 
   def edit
     project = @user.projects.find_by(id: params[:id])
-    render json: project
+    json_response(project)
   end
 
   def update
     project = Project.find_by(id: params[:id])
     project.update(project_params)
-    render json: project
+    json_response(project)
   end
 
   def destroy
     project = Project.find_by(id: params[:id])
     project.destroy
-    render json: project
+    json_response(project)
   end
 
   private
