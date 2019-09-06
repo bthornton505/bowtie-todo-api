e633f4c331434c87078aa05d39be1fe473a8a9be
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-06 12:01:07 -0600

    added corresponding messages for action responses

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,27 +1,22 @@
 class Api::V1::TodosController < ApplicationController
   before_action :set_project
 
-  def index
-    todos = @project.todos
-    json_response(todos)
-  end
-
   def create
     todo = @project.todos.create(todo_params)
-    json_response(todo)
+    json_response(message: Message.todo_created, todo: todo)
   end
 
   def update
     todo = Todo.find_by(id: params[:id])
-    todo.update(todo_params)
-    json_response(todo)
+    todo.update(completed: params[:completed])
+    json_response(message: Message.todo_updated, todo: todo)
   end
 
   def destroy
     todo = Todo.find_by(id: params[:id])
     todo.destroy
 
-    json_response(todo)
+    json_response(message: Message.todo_destroyed, todo: todo)
   end
 
   private
