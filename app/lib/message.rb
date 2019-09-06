ae522672a4fed69f6a333faf0e1a63e34121f834
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-06 12:01:32 -0600

    added todo server messages

--- app/lib/message.rb
+++ app/lib/message.rb
@@ -1,5 +1,6 @@
 class Message
 
+  # AUTHENTICATION MESSAGES
   def self.not_found(record = 'record')
     "Sorry, #{record} not found."
   end
@@ -20,6 +21,10 @@ class Message
     'Unauthorized Request'
   end
 
+  def self.authorized_user
+    "Authorized User"
+  end
+
   def self.account_created
     'Account created successfully'
   end
@@ -28,8 +33,30 @@ class Message
     'Account could not be created'
   end
 
+  def self.login_success
+    "Successful Login"
+  end
+
   def self.expired_token
     'Sorry, your token has expired. Please login to continue.'
+  end
+
+  # TODO MESSAGES
+  def self.todo_created
+    "Todo created successfully"
+  end
+
+  def self.todo_updated
+    "Todo successfully updated"
+  end
+
+  def self.todo_destroyed
+    "Todo destroyed"
+  end
+
+  # PROJECT MESSAGES
+  def self.project_destroyed
+    "Project destroyed"
   end
 
 end
