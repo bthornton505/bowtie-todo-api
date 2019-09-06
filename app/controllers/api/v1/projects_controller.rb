b2dc7accc8956cb8d7d546110309532cb79f4e00
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-06 13:16:34 -0600

    added all server messages and corresponding status

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,34 +3,39 @@ class Api::V1::ProjectsController < ApplicationController
 
   def index
     projects = @user.projects
-    json_response(projects)
+    json_response(message: Message.successful_request, projects: projects, status: 200)
   end
 
   def create
-    project = @user.projects.create(project_params)
-    json_response(project)
+    project = @user.projects.new(project_params)
+    if project.valid?
+      project.save
+      json_response(message: Message.project_created, project: project, status: 201)
+    else
+      errors = project.errors.messages
+      json_response(message: Message.project_failed, errors: errors, status: 400)
+    end
   end
 
   def show
-    project = @user.projects.find_by(id: params[:id])
-    json_response(project)
-  end 
-
-  def edit
     project = @user.projects.find_by(id: params[:id])
     json_response(project)
   end
 
   def update
     project = Project.find_by(id: params[:id])
-    project.update(project_params)
-    json_response(project)
+    if project.update(project_params)
+      json_response(message: Message.project_updated, project: project, status: 201)
+    else
+      errors = project.errors.messages
+      json_response(message: Message.update_failed, errors: errors, status: 400)
+    end
   end
 
   def destroy
     project = Project.find_by(id: params[:id])
     project.destroy
-    json_response(project)
+    json_response(message: Message.project_destroyed, project: project, status: 200)
   end
 
   private
