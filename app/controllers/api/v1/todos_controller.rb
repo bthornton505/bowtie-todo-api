f3a5aca84b5f0cd628ad36c35f2655201fbcaab0
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-06 13:21:22 -0600

    updated actions with server messages and corresponding status

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -2,21 +2,30 @@ class Api::V1::TodosController < ApplicationController
   before_action :set_project
 
   def create
-    todo = @project.todos.create(todo_params)
-    json_response(message: Message.todo_created, todo: todo)
+    todo = @project.todos.new(todo_params)
+    if todo.valid?
+      todo.save
+      json_response(message: Message.todo_created, todo: todo, status: 201)
+    else
+      errors = todo.errors.messages
+      json_response(message: Message.todo_failed, errors: errors, status: 400)
+    end
   end
 
   def update
     todo = Todo.find_by(id: params[:id])
-    todo.update(completed: params[:completed])
-    json_response(message: Message.todo_updated, todo: todo)
+    if todo.update(completed: params[:completed])
+      json_response(message: Message.todo_updated, todo: todo, status: 201)
+    else
+      errors = todo.errors.messages
+      json_response(message: Message.update_failed, errors: errors, status: 400)
+    end
   end
 
   def destroy
     todo = Todo.find_by(id: params[:id])
     todo.destroy
-
-    json_response(message: Message.todo_destroyed, todo: todo)
+    json_response(message: Message.todo_destroyed, todo: todo, status: 200)
   end
 
   private
