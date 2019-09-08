fa8851a4fe90088d9c297049633455222cbcbd67
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-08 00:53:41 -0600

    updated column type to text

--- db/migrate/20190902224257_create_projects.rb
+++ db/migrate/20190902224257_create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
-      t.string :title
+      t.text :title
       t.integer :user_id
 
       t.timestamps
